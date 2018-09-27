deff3826b2558ffdede78c044649479e433477b6
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-09-26 21:28:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 
 import RegistrationForm from './components/RegistrationForm';
 import Webpage from './components/Webpage';
-
+import FillerText from './components/FillerText';
 ReactDOM.render(
   (
     <div>
       <RegistrationForm />
       <Webpage />
+      <FillerText />
     </div>
   ),
   document.getElementById('global')
